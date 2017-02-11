e7fd240741e39b147a9d009c291cb82e528a4cdb
sgp715 <7SebastianPerez@gmail.com>
2017-02-11 02:06:13 -0600

    message to self

--- mail.py
+++ mail.py
@@ -5,6 +5,7 @@ import json
 with open('./config.json') as f:
     config = json.load(f)
 
+# http://naelshiab.com/tutorial-send-email-python/
 def send_email(message, to):
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
